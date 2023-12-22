9d5a04d522158f3d1b6889f6e8b2369a92f29ca9
Dafter <dafterplay@mail.ru>
2023-12-22 15:28:29 +0300

    fix names

--- main.py
+++ main.py
@@ -9,51 +9,51 @@ import eel
 # fix compile
 
 
-def find_all_names(input: str) -> set:
+def find_all_names(input_text: str) -> set:
     """
     Ищет все строки, которые начинаются без пробелов (это будут имена)
 
-    :param input: Текст
+    :param input_text: Текст
     :return: Множество из имен
     """
     line_with_name_pattern = re.compile(r'^([a-zA-Z0-9_]+).*$')
     names = {'ROOTER'}
-    for line in input.split('\n'):
+    for line in input_text.split('\n'):
         match = line_with_name_pattern.match(line)
         if match and match.group(1).lower() != 'GENERATE'.lower():
             names.add(match.group(1))
     return names
 
 
-def do_copy(input: str, postfix: str) -> str:
+def do_copy(input_text: str, postfix: str) -> str:
     """
     Выполняет копирование добавлением постфикса
 
-    :param input: Текст
+    :param input_text: Текст
     :param postfix: Постфикс
     :return: Обновленный текст
     """
-    all_names_in_GPSS = find_all_names(input)
+    all_names_in_text = find_all_names(input_text)
     names_pattern = re.compile(r'([a-zA-Z0-9_]+)')
     result = ""
-    split_line = re.split(names_pattern, input)
-    for name in split_line:
+    split_names = re.split(names_pattern, input_text)
+    for name in split_names:
         result += re.sub(r'\r?\n', '\r\n', name)
-        if name in all_names_in_GPSS or 'COLNUM' in name:
+        if name in all_names_in_text or 'COLNUM' in name:
             result += postfix
     return result
 
 
 @eel.expose
-def get_text_from_python(text: str, postfix: str) -> str:
+def get_text_from_python(input_text: str, postfix: str) -> str:
     """
     Функция для js, которая выполняет обновление текста
 
-    :param text: Текст
+    :param input_text: Текст
     :param postfix: Постфикс
     :return: Обновленный текст
     """
-    return do_copy(text, postfix)
+    return do_copy(input_text, postfix)
 
 
 def main():
